f6c7f0720b5826f93c0a944427e2b0ad9594fc5d
zoom-vmbb <79326806+zoom-vmbb@users.noreply.github.com>
2021-02-20 01:55:22 -0600

    Create codeSample3.php

---
+++ PSIRT/codeSample3.php
@@ -0,0 +1,18 @@
+The following changes were made to codeSample2 to mitigate issues the application was having with the
+username o’brien.
+
+<?php
+
+function escape_input($input) {
+return str_replace("'", "''", $input);
+}
+
+...
+
+$query = "SELECT user_id,usergroup FROM ".$config['db']['pre']."users WHERE
+username='" . escape_input($_POST['username']) . "' AND password='" .
+escape_input(md5($_POST['password'])) . "' AND status = '1' LIMIT 1";
+
+...
+
+?>
