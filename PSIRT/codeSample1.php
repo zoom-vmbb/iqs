3aaad44601c6a10bb00eccb415e3326412032702
zoom-vmbb <79326806+zoom-vmbb@users.noreply.github.com>
2021-02-20 01:48:42 -0600

    Create codeSample1.php

---
+++ PSIRT/codeSample1.php
@@ -0,0 +1,16 @@
+The following code is used to output the search results of a user-supplied query.
+
+<?php
+// Get the term we are searching for from the user
+$search_term = $_GET['query'];
+?>
+
+<html>
+<head>
+<title>Search Results for <?php echo $search_term; ?></title>
+</head>
+<body>
+  Here are your search results:
+  <?php get_search_results($search_term); ?>
+</body>
+</html>
