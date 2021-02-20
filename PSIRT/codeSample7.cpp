8353815c812fd6fa30dc8597fb43b49b79db3992
zoom-vmbb <79326806+zoom-vmbb@users.noreply.github.com>
2021-02-20 02:28:40 -0600

    Create codeSample7.cpp

---
+++ PSIRT/codeSample7.cpp
@@ -0,0 +1,15 @@
+The following code is compiled and set as a setuid binary in a Linux environment. It takes in two user-supplied
+string arguments and outputs the contents of buffers in memory.
+
+int main(int argc, char** argv) 
+{
+  char lower[] = “BBBBB”;
+  char low[] = “AAAAA”;
+  char buf[10];
+  strncpy(buf, argv[1], sizeof(buf));
+  strncat(buf, argv[2], sizeof(buf) – strlen(buf) – 1);
+  printf(“buf: %s\n”, buf);
+  printf(“low: %s\n”, low);
+  printf(“lower: %s\n”, lower);
+  return 0;
+}
