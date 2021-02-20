43758cc942db002f9042dee9f025c3491376c427
zoom-vmbb <79326806+zoom-vmbb@users.noreply.github.com>
2021-02-20 02:17:16 -0600

    Create codeSample4.php

---
+++ PSIRT/codeSample4.php
@@ -0,0 +1,28 @@
+The follow code is utilized to write a configuration file for the application. This configuration file is included in
+every page of the application to provide information needed to connect to the database.
+
+<?php
+  if(isset($_GET['save_config']))
+  {
+    // Content that will be written to the config file
+    $content = "<?php\n";
+    // Interate each POST parameter and write the config file
+    
+    foreach($_POST as $param_name => $value) {
+      $content.= "\$config['db']['".$param_name."'] = '".addslashes($value)."';\n";
+    }
+    
+    $content.= "?>";
+
+    // Open the WEB_ROOT/includes/config.php for writing
+    $handle = fopen('../includes/config.php', 'w');
+
+    // Write the config file
+    fwrite($handle, $content);
+
+    // Close the file
+    fclose($handle);
+    header("Location: template_settings.php");
+    exit;
+  }
+?>
