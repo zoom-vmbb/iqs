7df0a05030d6651c7ccbef0b7c532190083146ad
zoom-vmbb <79326806+zoom-vmbb@users.noreply.github.com>
2021-02-20 02:32:20 -0600

    Create codeSample8.cpp

---
+++ PSIRT/codeSample8.cpp
@@ -0,0 +1,28 @@
+This snippet of code is used to receive a user-supplied packet from a network connection.
+
+#define HEADER_SIZE sizeof(int)
+#define MAX_DATA 256
+
+struct packet_struct 
+{
+  int data_length;
+  char* data[MAX_DATA];
+} buf;
+  
+if((read(sock, &buf, HEADER_SIZE)) < 0)
+{
+  perror("read");
+  exit(errno);
+}
+
+if (buf.data_length > MAX_DATA) 
+{
+  perror("too large");
+  exit(errno);
+}
+
+if((read(sock, &buf.data, buf.data_length)) < 0)
+{
+  perror("read");
+  exit(errno);
+}
